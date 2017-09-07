5ffde658c7412935be419a651253f8a51c059852
John <16932417+torrencj@users.noreply.github.com>
2017-09-07 18:26:14 -0500

    finished main functionality

--- js/app.js
+++ js/app.js
@@ -1,81 +1,82 @@
-//Requires jquery
+//This file requires jquery!
 var input = document.querySelector('input');
 var inputForm = document.querySelector('form');
 var inputTxt = document.querySelector('.txt');
 var lastSent;
 
-//speech recognition stuff
+//speech recognition vars.
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
 
 
-/*
-Mic button stuff.
-*/
+/*********************************************************************************************
+START speech recognition
+*********************************************************************************************/
+
 var micBtn = document.querySelector('#micButton');
+var micIcon = document.querySelector('#micIcon');
 
-function testSpeech() {
-
+function startSpeech() {
   micBtn.disabled = true;
-  micBtn.textContent = 'Listening...';
+  micIcon.textContent = 'hearing';
+  $('#micButton').toggleClass('pulse');
+  $('#micButton').addClass('red');
   var recognition = new SpeechRecognition();
   recognition.lang = 'en-US';
   recognition.interimResults = false;
   recognition.maxAlternatives = 1;
 
-  // Init the browser's own Speech Recognition
-  // var recognition = new webkitSpeechRecognition();
-
   recognition.start();
 
   recognition.onresult = function(event) {
     console.log(event.results);
-
     var speechResult = event.results[0][0].transcript;
     inputTxt.value = speechResult;
     console.log(speechResult);
-
     console.log('Confidence: ' + event.results[0][0].confidence);
+    $('.txt').submit();
   }
 
   recognition.onspeechend = function() {
     recognition.stop();
     micBtn.disabled = false;
-    micBtn.textContent = 'Use your voice!';
+    micIcon.textContent = 'keyboard_voice';
+    $('#micButton').toggleClass('pulse');
+    $('#micButton').toggleClass('red');
   }
 
   recognition.onerror = function(event) {
     micBtn.disabled = false;
-    micBtn.textContent = 'Start new test';
-    // diagnosticPara.textContent = 'Error occurred in recognition: ' + event.error;
+    micIcon.textContent = 'keyboard_voice';
+    $('#micButton').toggleClass('pulse');
+    // diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
   }
-
 }
 
-micBtn.addEventListener('click', testSpeech);
+micBtn.addEventListener('click', startSpeech);
 
-/*
-End mic button stuff
-*/
-
-
+/*********************************************************************************************
+END speech recognition
+*********************************************************************************************/
 
 inputForm.onsubmit = function(event) {
+
+  $('#loading-results').toggleClass('hidden');
   $('.highlight').removeClass('highlight');
   event.preventDefault();
   console.log(inputTxt.value);
   lastSent = inputTxt.value;
-  // console.log(event);
   inputTxt.blur();
-
   $.ajax({
     url:"https://104.154.104.227:8000/parse?s=" + inputTxt.value,
     method:"GET"
   }).done(function(response) {
+    $('#loading-results').toggleClass('hidden')
     console.log(response);
-    console.log(response.classification);
-    $('#suggestion').text(response.classification);
+    handle(response);
+    $('#category').text(response.category);
+    $('#search-string').text(response.search);
     $('#' + response.classification).addClass('highlight');
   });
   $('#speechstring').text(inputTxt.value);
@@ -85,11 +86,8 @@ inputForm.onsubmit = function(event) {
   $('.clickable').click(function(event) {
     console.log(event.currentTarget);
     if (lastSent) {
-      //send stuff
-
       console.log(lastSent);
       console.log(event.currentTarget.id);
-      // inputTxt.blur();
       $.ajax({
         url:"https://104.154.104.227:8000/parse?s=" + lastSent + "&c=" + event.currentTarget.id,
         method:"POST"
@@ -97,9 +95,74 @@ inputForm.onsubmit = function(event) {
         console.log(response);
       });
       lastSent = '';
+      Materialize.toast("Thanks! I'll get it right next time.", 4000)
     } else {
       // do nothing
     }
-    //TODO modial popup saying thanks.
-
   });
+
+function handle(response) {
+  if (response.category) { //if it exists at all.
+    $("#interaction-panel").toggleClass('hidden');
+    if (response.category == 'google') {
+      $('#target').fadeOut();
+      $('#target').empty();
+
+      for (var i = 0; i < response.result.length; i++) {
+        if (response.result[i].title && response.result[i].href && response.result[i].desc) {
+          googleGen(response.result[i].title, response.result[i].href, response.result[i].desc);
+        }
+      }
+      $('#target').fadeIn();
+    } else if (response.category == 'twitter') {
+      $('#target').fadeOut();
+      $('#target').empty();
+      for (var i = 0; i < response.result.length; i++) {
+        twitterGen(response.result[i].userName, response.result[i].userImg, response.result[i].text);
+      }
+      $('#target').fadeIn();
+
+    }else if (response.category == 'omdb') {
+      omdbGen(response.result.Poster, response.result.Title, response.result.imdbID)
+    }else { //spotify
+      spotifyGen(response.result["0"].uri);
+    }
+  } else {
+    console.log("Something went wrong.");
+  }
+}
+
+
+
+function googleGen(title, href, desc){
+  $("#target").append(
+    '<ul class="collection">'
+    +'<li class="collection-item">'
+    +'<span class="title"><h5><a href='+href+'>'+title+'</a></h5></span>'
+    +'<p>'+href+'</p>'
+    +'<p>'+desc+'</p>'
+    +'</li>'
+    +'</ul>'
+  )
+}
+
+function twitterGen(userName, userImage, text) {
+  $('#target').append('<ul class="collection">'
+    + '<li class="collection-item avatar">'
+    + '<img src='+userImage+' alt="" class="circle">'
+    + '<span class="title">'+userName+'</span>'
+    + '<p>'+text+'</p></li></ul>')
+}
+
+function omdbGen(posterURI, title, imdb) {
+  if (posterURI == "N/A") {
+    posterURI = 'http://via.placeholder.com/200x300?text=N/A';
+  }
+  $('#target').html('<div class="card col s4 offset-s4"><div class="card-image"><img src='+posterURI
+  +'></div><div class="card-content"><p>'+title+'</p></div><div class="card-action">'
+  + '<a href="http://www.imdb.com/title/'+imdb+'/">View on IMDB</a></div></div>')
+}
+
+function spotifyGen(uri) {
+  $('#target').html('<iframe src="https://open.spotify.com/embed?uri=' + uri + '" frameborder="0" allowtransparency="true"></iframe>');
+}
